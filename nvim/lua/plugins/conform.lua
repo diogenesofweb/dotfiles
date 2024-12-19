5c079a790b63c2a2daf60e04cb45fca055b2c321
Diogenes <diogenesofweb@gmail.com>
2024-12-19 16:41:10 +0200

    Up: 2024-12-19 16:41:10

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -4,15 +4,15 @@ return {
   cmd = { "ConformInfo" },
   opts = {
     formatters_by_ft = {
-      ["javascript"] = { { "prettierd", "prettier" } },
-      ["javascriptreact"] = { { "prettierd", "prettier" } },
-      ["typescript"] = { { "prettierd", "prettier" } },
-      ["typescriptreact"] = { { "prettierd", "prettier" } },
+      ["javascript"] = { "prettierd", "prettier", stop_after_first = true },
+      ["javascriptreact"] = { "prettierd", "prettier", stop_after_first = true  },
+      ["typescript"] = { "prettierd", "prettier", stop_after_first = true },
+      ["typescriptreact"] = {  "prettierd", "prettier", stop_after_first = true  },
       -- ["svelte"] = { { "prettierd", "prettier" } },
-      ["css"] = { { "prettierd", "prettier" } },
-      ["html"] = { { "prettierd", "prettier" } },
-      ["json"] = { { "prettierd", "prettier" } },
-      ["markdown"] = { { "prettierd", "prettier" } },
+      ["css"] = { "prettierd", "prettier", stop_after_first = true },
+      ["html"] = { "prettierd", "prettier", stop_after_first = true },
+      ["json"] = { "prettierd", "prettier", stop_after_first = true },
+      ["markdown"] = { "prettierd", "prettier", stop_after_first = true },
     },
 
     -- format_on_save = {
